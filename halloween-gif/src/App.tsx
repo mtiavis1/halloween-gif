7a07f50f92a9f9b64d1462a2bbd128e0347d542d
mtiavis1 <mtiavis1@jh.edu>
2021-10-19 14:56:48 -0400

    Adding halloween base files

---
+++ halloween-gif/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header.js'
+import './app.css'
+
+
+function App() {
+  return (
+
+    <body id="background">
+      <Header></Header>
+      <Header></Header>
+    </body>
+  );
+}
+
+export default App;
---
+++ halloween-gif/src/components/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <div>Hello Mark!</div>
+    );
+  }
+  
+export default Header;
