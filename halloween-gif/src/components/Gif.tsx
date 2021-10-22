3df6fd3d139a09a616fbf607a8c9bda121e241c4
mtiavis1 <mtiavis1@jh.edu>
2021-10-22 09:47:56 -0400

    created dispatch

--- halloween-gif/src/components/Gif.tsx
+++ halloween-gif/src/components/Gif.tsx
@@ -1,18 +1,20 @@
 import '../app.css';
-
-import React, { useState } from 'react'
-
 import { useAppSelector, useAppDispatch } from '../hook'
 
 import { decrement, increment } from '../changeGif'
 
-
 function Gif() {
-  const index = useAppSelector((state) => state.indexer.index);
+  const index = useAppSelector((state) => state.indexer);
   console.log(index);
+
+  const dispatch = useAppDispatch();
+  
   const titleName:string = "HallowSurprise!";
     return (
-        <div id='title'>{titleName}</div>
+        <div id='title'>{titleName}
+        <button onClick={() => dispatch(increment())}>increment</button>
+       <button onClick={() => dispatch(decrement())}>decrement</button>
+        </div>
     );
   }
   
